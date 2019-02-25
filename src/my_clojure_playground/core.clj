3121c3834d5778752baa2c4676fb9a8c0f788c38
jetimmins <jaytimmins@gmail.com>
2019-02-25 15:01:22 +0000

    added first part of core, linked REPL

--- src/my_clojure_playground/core.clj
+++ src/my_clojure_playground/core.clj
@@ -4,3 +4,31 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(def objects '(whiskey-bottle bucket frog chain))
+
+(def game-map (hash-map
+                'living-room '((you are in the living room
+                                    of a wizards house - there is a wizard
+                                    snoring loudly on the couch -)
+                                (west door garden)
+                                (upstairs stairway attic))
+                'garden '((you are in a beautiful garden -
+                               there is a well in front of you -)
+                           (east door living-room))
+                'attic '((you are in the attic of the
+                              wizards house - there is a giant
+                              welding torch in the corner -)
+                          (downstairs stairway living-room))))
+
+(def object-locations (hash-map
+                        'whiskey-bottle 'living-room
+                        'bucket 'living-room
+                        'chain 'garden
+                        'frog 'garden))
+
+(def location 'living-room)
+
+(defn describe-location [location game-map]
+  (first (location game-map)))
+
