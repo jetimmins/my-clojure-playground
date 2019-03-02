eac8e3cd81aaf2d9717aab4d8c18ca36daff0d38
jetimmins <jaytimmins@gmail.com>
2019-03-02 23:30:43 +0000

    complete pre-refactoring

--- src/my_clojure_playground/core.clj
+++ src/my_clojure_playground/core.clj
@@ -56,3 +56,53 @@
           :else '(you cannot go that way -))))
 
 (defspel walk [direction] `(walk-direction '~direction))
+
+(defn pickup-object [object]
+  (cond (is-at? object location object-locations)
+        (do
+          (def object-locations (assoc object-locations object 'body))
+          `(you are now carrying the ~object))
+        :else '(you cannot get that.)))
+
+(defspel pickup [object] `(spel-print (pickup-object '~object)))
+
+(defn inventory []
+  (filter (fn [x] (is-at? x 'body object-locations)) objects))
+
+(defn have? [object]
+  (some #{object} (inventory)))
+
+(def chain-welded false)
+
+(def bucket-filled false)
+
+(defspel game-action [command subj obj place & args]
+         `(defspel ~command [subject# object#]
+                   `(spel-print (cond (and (= location '~'~place)
+                                           (= '~subject# '~'~subj)
+                                           (= '~object# '~'~obj)
+                                           (have? '~'~subj))
+                                      ~@'~args
+                                      :else '(i cannot ~'~command like that -)))))
+
+(game-action weld chain bucket attic
+             (cond (and (have? 'bucket) (def chain-welded true))
+                   '(the chain is now securely welded to the bucket -)
+                   :else '(you do not have a bucket -)))
+
+(game-action dunk bucket well garden
+             (cond chain-welded
+                   (do (def bucket-filled true)
+                       '(the bucket is now full of water))
+                   :else '(the water level is too low to reach -)))
+
+(game-action splash bucket wizard living-room
+             (cond (not bucket-filled) '(the bucket has nothing in it -)
+                   (have? 'frog) '(the wizard awakens and sees that you stole
+                                       his frog -
+                                       he is so upset he banishes you to the
+                                       netherworlds - you lose! the end -)
+                   :else '(the wizard awakens from his slumber and greets you
+                               warmly -
+                               he hands you the magic low-carb donut - you win!
+                               the end -)))
